5ec5e087e490f7896ef07e730b872b197c599ab7
John Cassidy <jpcassidy@outlook.com>
2022-02-15 11:46:12 -0500

    create sign-in page/component and form-input component and button component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import React from 'react';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
         {/* <Route exact path='/topics' component={TopicsList}/>
         <Route path='/topics/:topicId' component={TopicDetail}/> */}
       </Switch>
