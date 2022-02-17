bf6e9484c80ca649ef11d35b6dcb80d8ab45bf59
John Cassidy <jpcassidy@outlook.com>
2022-02-17 16:17:33 -0500

    implement signin/signup and insert user into firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,11 +2,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 import firebase from 'firebase/compat/app';
+import { useRef } from 'react';
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: `${process.env.REACT_APP_FIREBASE_AUTHDOMAIN}`,
-  //   databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASEURL}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASEURL}`,
   projectId: `${process.env.REACT_APP_FIREBASE_PROJECTID}`,
   storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}`,
   messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID}`,
@@ -14,6 +15,31 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
