0ce1b3722a0dd7398368a122004744b2418991a4
John Cassidy <jpcassidy@outlook.com>
2022-03-15 11:22:34 -0400

    create ErrorBoundary and wrap page routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense, lazy, useEffect } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
 import { GlobalStyle } from './global.styles';
 import Header from './components/header/header.component';
 import Spinner from './components/spinner/spinner.component';
@@ -33,19 +34,21 @@ const App = () => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Suspense fallback={<Spinner />}>
-          <Route exact path='/' component={HomePage} />
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path='/' component={HomePage} />
 
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route
-            exact
-            path='/signin'
-            render={() =>
-              currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />
-            }
-          />
-        </Suspense>
+            <Route path='/shop' component={ShopPage} />
+            <Route exact path='/checkout' component={CheckoutPage} />
+            <Route
+              exact
+              path='/signin'
+              render={() =>
+                currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />
+              }
+            />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
