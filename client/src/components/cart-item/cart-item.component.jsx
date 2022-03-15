a70ddcd075b04196823885e8326bead11785b5d8
John Cassidy <jpcassidy@outlook.com>
2022-03-15 11:37:04 -0400

    cartitem component add React.memo

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,7 +1,7 @@
 import {
   CartItemContainer,
-  ItemDetailsContainer,
   CartItemImage,
+  ItemDetailsContainer,
 } from './cart-item.styles';
 
 import React from 'react';
@@ -18,4 +18,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   </CartItemContainer>
 );
 
-export default CartItem;
+export default React.memo(CartItem);
